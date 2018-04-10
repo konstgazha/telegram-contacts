e3c7f3466936b2b34da3287cfa5a5f72937f9cb9
Konstantin Gazha <konstgazha@yandex.ru>
2018-04-10 16:58:01 +0500

    Fix method for getting contacts from groups

--- contacts.py
+++ contacts.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 import crawlers
+import pickle
+import csv
+import re
+import time
 
 
 class Person:
@@ -19,27 +23,54 @@ class Person:
     def get_phone(self):
         return self.phone
 
+    def filter_phone(self):
+        if not re.findall('\+[\d].+', self.phone):
+            self.phone = ''
+    
 
 class TelegramContact:
     def __init__(self):
         self.telegram_url = "https://web.telegram.org/#/login"
 
+    def save_contacts(self, row, filename):
+        with open(filename, 'w', newline='') as csvfile:
+            contacts = csv.writer(csvfile)
+            for row in info:
+                contacts.writerow([row.name, row.phone])
+        
     def get_contacts(self):
         crawler = crawlers.SeleniumCrawler()
         crawler.driver.get(self.telegram_url)
-        print("Press any key to continue")
-        input()
-        
-        persons = []
-        contacts = crawler.driver.find_elements_by_xpath("//a[@class='md_modal_list_peer_name']")
-        for contact in contacts:
-            contact.click()
-            name = crawler.driver.find_elements_by_class_name('peer_modal_profile_name')[-1].text
-            phone = crawler.driver.find_elements_by_class_name('peer_modal_profile_name')[0].text
-            persons.append(Person(name, phone))
-            crawler.driver.find_elements_by_class_name('md_modal_action_close')[-1].click()
+        i = 0
+        db = []
+        while True:
+            print("Press any key to continue, 'e' to exit")
+            key = input()
+            if key == 'e':
+                break
+            persons = []
+            try:
+                contacts = crawler.driver.find_elements_by_xpath("//a[@class='md_modal_list_peer_name']")
+                for contact in contacts:
+                    contact.click()
+                    time.sleep(1)
+                    name = crawler.driver.find_elements_by_class_name('peer_modal_profile_name')[-1].text
+                    phone = ''
+                    try:
+                        phone = crawler.driver.find_elements_by_xpath("//div[@class='md_modal_section_param_value']")[0].text
+                    except Exception as ex:
+                        # print(ex)
+                        pass
+                    persons.append(Person(name, phone))
+                    crawler.driver.find_elements_by_class_name('md_modal_action_close')[-1].click()
+            except Exception as ex:
+                # print(ex)
+                pass
             
-        return persons
+            self.save_contacts('contacts' + str(i) + '.csv')
+            i += 1
+            db.append(persons)
+        return db
 
 
 tc = TelegramContact()
